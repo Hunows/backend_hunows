f42cf5d56be820380b6bc5dc6fba961c89afd542
Jessica Moraes <jm.jeska@gmail.com>
2023-02-07 15:52:41 -0300

    Adicionando novas rotas

--- forum_hunows/src/routes/userRoutes.js
+++ forum_hunows/src/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const UserController = require('../controllers/UserController');
+const protect = require('../middlewares/authentication');
 
 module.exports = userRouter = Router();
 
 const userController = new UserController();
-userRouter.post('/register', userController.registerAccount);
+userRouter.route("/register").post(userController.registerAccount);
+userRouter.route("/users").get(protect, userController.all);
+userRouter.route("/login").post(userController.userLogin);
