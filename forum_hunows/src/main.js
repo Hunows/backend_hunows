25b360824fefca9c2e13b60e9b440935e5ec40c5
Jessica Moraes <jm.jeska@gmail.com>
2023-02-07 10:13:24 -0300

    Corrigino nome do banco de dados e alterando porta

--- forum_hunows/src/main.js
+++ forum_hunows/src/main.js
@@ -1,11 +1,11 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/users')
+mongoose.connect('mongodb://localhost:27017/hunowsdb')
     .then(() => {
         console.log('Conectou ao banco!')
-        app.listen(3000, function () {
-            console.log("Accounts server is running in port 3000");
+        app.listen(3007, function () {
+            console.log("Accounts server is running in port 3001");
         })
     })
     .catch((err) => console.log(err));
