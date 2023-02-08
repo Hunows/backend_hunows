845efbbc69a812c301ef71fab846b175508d8907
Jessica Moraes <jm.jeska@gmail.com>
2023-02-07 21:59:53 -0300

    Adicionando validação na rota de registro de usuário

--- forum_hunows/src/routes/userRoutes.js
+++ forum_hunows/src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const UserController = require('../controllers/UserController');
 const protect = require('../middlewares/auth');
+const userValidations = require('../schemas/userSchema');
 
 const router = express.Router();
 
-router.route("/").post(UserController.userRegister);
+router.route("/").post(userValidations, UserController.userRegister);
 router.route("/login").post(UserController.userLogin);
 router.route("/").get(protect, UserController.getAllUsers);
 
