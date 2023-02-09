a3005e48b9d3690b132bbabbbe803e3e819fdad9
Jessica Moraes <jm.jeska@gmail.com>
2023-02-08 21:29:20 -0300

    Corrigindo sintaxe

--- forum_hunows/src/middlewares/auth.js
+++ forum_hunows/src/middlewares/auth.js
@@ -4,20 +4,20 @@ const User = require("../models/User");
 const protect = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    return res.status(401).json({ message: "Você não passou o token" });
+  if(!authHeader) {
+    return res.status(401).json({ message: 'Você não passou o token' })
   }
 
   const parts = authHeader.split(" ");
 
-  if (parts.length !== 2) {
-    return res.status(401).json({ message: "O tipo di token é inváldo" });
+  if(parts.length !== 2) {
+    return res.status(401).json({ message: 'O tipo di token é inváldo' })
   }
 
-  const [scheme, token] = parts;
+  const[scheme, token] = parts;
 
-  if (scheme.indexOf("Bearer") !== 0) {
-    return res.status(401).json({ message: "Token malformatado" });
+  if(scheme.indexOf("Bearer") !== 0) {
+    return res.status(401).json({ message: 'Token malformatado' })
   }
 
   jwt.verify(token, "kmdKw9ehknwDMn29w2whn2oewQWK", (err, decoded) => {
@@ -26,5 +26,7 @@ const protect = async (req, res, next) => {
     req.userId = decoded.id;
     return next();
   })
+
+}
 
 module.exports = protect;
