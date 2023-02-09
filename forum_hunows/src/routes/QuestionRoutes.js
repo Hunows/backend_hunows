681e7060ad71021658be646cf2f6dbddbf242292
Jessica Moraes <jm.jeska@gmail.com>
2023-02-09 20:16:57 -0300

    Corrigindo rota put

--- forum_hunows/src/routes/QuestionRoutes.js
+++ forum_hunows/src/routes/QuestionRoutes.js
@@ -8,6 +8,6 @@ const questionController = new QuestionController();
 questionRouter.route("/registerAsk").post(protect, questionController.registerAsk);
 questionRouter.route("/QuestionLists").get(questionController.getAllQue);
 questionRouter.route("/QuestionLists/:id").get(questionController.getAllId);
-questionRouter.route("/Question/:id").put(protect, questionController.registerStatus);
+questionRouter.route("/Question/:id/Answer/:id").put(protect, questionController.registerStatus);
 
 
